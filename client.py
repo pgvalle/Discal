fb70729c9342ba00a58f1f2ac834bee1ff5d8e9d
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-28 11:48:27 -0300

    refactoring client. Added my send and recv to shorten calls to those primitives

--- client.py
+++ client.py
@@ -1,34 +1,32 @@
 from common import *
 
 if len(sys.argv) != 3:
-    print('Pass ip and port as arguments!')
-    print('Example: python client.py 127.0.0.1 5000')
-    quit()
+  print('Pass ip and port as arguments!')
+  print('Example: python client.py 127.0.0.1 5000')
+  quit()
 
 ip, port = sys.argv[1], int(sys.argv[2])
 
 def loop():
-    try:
-        while True:
-            a = float(input('type first number: '))
-            op = input('type operation: ')
-            b = float(input('type second number: '))
+  try:
+    while True:
+      a = float(input('type first number: '))
+      op = input('type operation: ')
+      b = float(input('type second number: '))
 
-            msg = json.dumps({ 'a': a, 'b': b, 'op': op })
-            encoded_msg = msg.encode(ENCODING)
+      req = json.dumps({ 'a': a, 'b': b, 'op': op })
 
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4 tcp socket
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4 tcp socket
+      sock.connect((ip, port))
+      send(sock, req)
 
-            sock.connect((ip, port))
-            sock.send(encoded_msg)
-            encoded_rsp = sock.recv(CHUNK)
-            rsp = rsp.decode(ENCODING)
-            sock.close()
+      rsp = recv(sock)
+      print(rsp)
 
-            print(f'result: {rsp['result']}')
-    except KeyboardInterrupt:
-        print('\nBye')
-    except Exception as e:
-        print(e)
+      sock.close()
+  except KeyboardInterrupt:
+    print('\nBye')
+  except Exception as e:
+    print(e)
 
 loop()
--- common.py
+++ common.py
@@ -3,3 +3,10 @@ import sys, site, socket, json
 CHUNK = 1024
 ENCODING = 'utf-8'
 
+def send(sock, msg):
+  encoded_msg = msg.encode(ENCODING)
+  return sock.send(encoded_msg)
+
+def recv(sock):
+  encoded_msg = sock.recv(CHUNK)
+  return encoded_msg.decode(ENCODING)
