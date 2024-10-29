7f92bae5952df895c3e955816166603b07cfbfa1
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-28 21:08:45 -0300

    Brainstorming ideas to implement load balancing

--- balancer.py
+++ balancer.py
@@ -1,1 +1,8 @@
 from common import *
+
+'''guardar registros de tempos em tempos (na faixa de segundos)
+do uso da cpu em cada servidor. Depois fazer algum cálculo em cima
+desses valores pra decidir qual servidor vai ser usado'''
+
+'''Threads ou Forks para tratar as conexões com os clientes???'''
+
