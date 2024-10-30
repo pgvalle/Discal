ae62b3d45f854c22b55ac7a87ce933ded16aa52f
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-30 13:10:10 -0300

    blocking socket.accept for nonblocking socket

--- common.py
+++ common.py
@@ -1,12 +1,29 @@
-import sys, site, socket, json
+import sys
+import socket
+import json
 
 CHUNK = 1024
 ENCODING = 'utf-8'
 
+# Nonblocking accept regardless of socket.setblocking.
+# With this only recv and send are blocking.
+def accept(sock):
+  while True:
+    try:
+      return sock.accept()
+    except socket.timeout:
+      pass
+
 def send(sock, msg):
-  encoded_msg = msg.encode(ENCODING)
-  return sock.send(encoded_msg)
+  try:
+    msg = msg.encode(ENCODING)
+    return sock.send(msg)
+  except Exception as e:
+    print(e)
 
 def recv(sock):
-  encoded_msg = sock.recv(CHUNK)
-  return encoded_msg.decode(ENCODING)
+  try:
+    msg = sock.recv(CHUNK)
+    return msg.decode(ENCODING)
+  except Exception as e:
+    print(e)
