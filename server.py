2b0615360b57d4ebe8cfc67c9fb2b376011b47b1
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-30 13:11:12 -0300

    Child connection handler processes terminating properly

--- server.py
+++ server.py
@@ -1,92 +1,97 @@
 from common import *
 import os
+import signal
 import psutil
 
 
-# must have ip, port1 and port2
-if len(sys.argv) != 4:
-  print('Pass ip, calc_port and cpu_port as arguments!')
-  print('Example: python server.py 127.0.0.1 5000 5001')
-  quit()
+def sigchld_handler(num, bruh):
+  os.wait()
 
-# port1 is the calculator service port. port2 is the cpu usage service port
-ip, port1, port2 = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
 
+# Entrypoint
+
+def main():
+  # Must have ip, port1 and port2
+  if len(sys.argv) != 4:
+    print('Pass ip, calc_port and cpu_port as arguments!')
+    print('Example: python server.py 127.0.0.1 5000 5001')
+    return
+
+  # Port1 is for calculator service. Port2 is for the cpu usage service.
+  ip, port1, port2 = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+
+  try:
+    if os.fork() == 0:  # Child. CPU usage service.
+      cpu_usage(ip, port2)
+    else:  # Parent. Calculator service.
+      calculator(ip, port1)
+  except KeyboardInterrupt:
+    pass
 
 # calculator service
 
-def calculate(req):
+def calculator(ip, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.settimeout(1)
+  sock.bind((ip, port))
+  sock.listen()
+
+  # This fixes zombie child processes never terminating
+  signal.signal(signal.SIGCHLD, sigchld_handler)
+
+  def calculator_rsp(req):
+    rsp = { 'status': 0 }
     try:
-      req_dict = json.loads(req)
-      a = req_dict['a']
-      b = req_dict['b']
-      op = req_dict['op']
-      result = eval(f'{a} {op} {b}')
+      req = json.loads(req)
+      expr = f'{req["a"]} {req["op"]} {req["b"]}'
 
-      return { 'status': 0, 'result': result }
+      rsp['result'] = eval(expr)
     except Exception as e:
-      return { 'status': 1, 'result': str(e) }
+      rsp['status'] = 1
+      rsp['result'] = str(e)
 
-def calculator():
-  l = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  l.bind((ip, port1))
-  l.listen()
+    return json.dumps(rsp)
 
-  try:
-    while True:
-      conn, addr = l.accept()
+  while True:
+    conn, addr = accept(sock)
+    print(f'Received connection from {addr} to calculate something.')
 
-      # Child process. Receive request, respond then exit.
-      if os.fork() == 0:
-        req = recv(conn)
-        print(f'Received {req} from {addr}', end='. ')
+    if os.fork() == 0:  # Child. Receive request, respond and done.
+      req = recv(conn)
+      print(f'Received {req} from {addr}.')
 
-        rsp_dict = calculate(req)
-        rsp = json.dumps(rsp_dict)
-        print(f'Responding with {rsp}.')
+      rsp = calculator_rsp(req)
+      send(conn, rsp)
+      print(f'Responded {addr} with {rsp}.')
 
-        # Network errors may happen.
-        try:
-          send(conn, rsp)
-        except Exception as e:
-          print(e)
-
-        conn.close()
-        break
-  except KeyboardInterrupt:
-    l.close()
+      conn.close()
+      os._exit(0)
 
 
 # cpu usage service
 
-def cpu_usage():
-  l = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  l.bind((ip, port2))
-  l.listen()
+def cpu_usage(ip, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.settimeout(1)
+  sock.bind((ip, port))
+  sock.listen()
 
-  try:
-    while True:
-      conn, addr = l.accept()
+  # This fixes zombie child processes never terminating
+  signal.signal(signal.SIGCHLD, sigchld_handler)
 
-      # Child process. Send cpu usage then exit.
-      if os.fork() == 0:
-        usage = psutil.cpu_percent(interval=None)
-        print(f'Telling {addr} CPU usage now is {usage}%')
+  while True:
+    conn, addr = accept(sock)
+    print(f'Received connection from {addr} to get CPU usage.')
 
-        # Network errors may happen.
-        try:
-          send(conn, str(usage))
-        except Exception as e:
-          print(e)
+    if os.fork() == 0:  # Child. Send cpu usage and done.
+      usage = psutil.cpu_percent(interval=None)
+      usage = str(usage)
+      send(conn, usage)
+      print(f'Sent {addr} current CPU usage: {usage}%.')
 
-        conn.close()
-        break
-  except KeyboardInterrupt:
-    l.close()
+      conn.close()
+      os._exit(0)
 
 
 if __name__ == '__main__':
-  if os.fork() == 0:
-    cpu_usage()
-  else:
-    calculator()
+  main()
